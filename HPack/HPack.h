e00ea6980ba9553f57eb18961af30754bc2bed1b
maitegm <maite.gonzalez.mendoza@gmail.com>
2018-09-06 17:40:19 -0300

    added functions to search header in tables

--- HPack/HPack.h
+++ HPack/HPack.h
@@ -32,6 +32,8 @@ public:
 	bool resize(uint32_t size);
 
 	static void operator delete(void *ptr);
+
+	uint32_t search_header(char* name, char* value);
 private:
 	uint32_t first;
 	uint32_t next;
@@ -102,10 +104,12 @@ private:
 class HeaderBuffer{
 public:	
 	HeaderBuffer(uint32_t incomming_buffer_max_size, uint32_t maxSize);
-
+	
 	static void operator delete(void *ptr);
 
-	static HeaderPair* const static_table[];
+	
+
+
 	HPackDynamicTable * dyn_table;
 
 	
@@ -129,9 +133,6 @@ public:
 	bool isHuffmanEncoded(uint32_t pointer);
 
 	HPackData* getNext();
-
-
-
 	char* toString();
 
 	uint32_t decodeInteger(uint32_t pointer, uint8_t prefix);
@@ -141,8 +142,9 @@ public:
 	void indexData(HPackData* data);
 	EncodedData* createHeaderBlock(HPackData** hp_datas, uint32_t size);
 	uint32_t receiveHeaderBlock(byte* header_block, uint32_t size);
-private:
 	HeaderPair* findEntry(uint32_t index);
+private:
+	
 	uint32_t buf_size;
 	byte *buf;// = new byte[1000];
 	uint32_t first; 
@@ -161,6 +163,15 @@ class HPack{
 public:
 	HPack(uint32_t max_header_buffer_size, uint32_t max_header_table_size);
 	HeaderBuffer* hb;
+	uint32_t search_header(char* name, char* value);
+	uint32_t search_header_in_static_table(char* name, char* value);
+	uint32_t search_header_in_dyn_table(char* name, char* value);
+
+	HeaderPair * findEntry(uint32_t index);
+
+	EncodedData* encode(char *name, bool nameHuffman, char* value, bool valueHuffman, bool indexing, bool neverIndexed);
+	void decode(char* encoded_data, uint32_t encoded_size);
+	HeaderPair* getNextHeaderPair();
 
 	static void operator delete(void *ptr);
 private:
@@ -168,6 +179,8 @@ private:
 	uint32_t max_header_buffer_size;
 	uint32_t dyn_table_size;
 	uint32_t max_header_table_size;
+
+	uint32_t binary_search_static_table(char*name, char*value, int start, int end);
 };
 
 
@@ -207,49 +220,47 @@ const char name_24[] PROGMEM = "content-disposition";
 const char name_25[] PROGMEM = "content-encoding";
 
 const char name_26[] PROGMEM = "content-language";
-const char name_27[] PROGMEM = "content-language";
-const char name_28[] PROGMEM = "content-length";
-const char name_29[] PROGMEM = "content-location";
-const char name_30[] PROGMEM = "content-range";
+const char name_27[] PROGMEM = "content-length";
+const char name_28[] PROGMEM = "content-location";
+const char name_29[] PROGMEM = "content-range";
+const char name_30[] PROGMEM = "content-type";
 
-const char name_31[] PROGMEM = "content-type";
-const char name_32[] PROGMEM = "cookie";
-const char name_33[] PROGMEM = "date";
-const char name_34[] PROGMEM = "etag";
-const char name_35[] PROGMEM = "expect";
+const char name_31[] PROGMEM = "cookie";
+const char name_32[] PROGMEM = "date";
+const char name_33[] PROGMEM = "etag";
+const char name_34[] PROGMEM = "expect";
+const char name_35[] PROGMEM = "expires";
 
-const char name_36[] PROGMEM = "expires";
-const char name_37[] PROGMEM = "from";
-const char name_38[] PROGMEM = "host";
-const char name_39[] PROGMEM = "if-match";
-const char name_40[] PROGMEM = "if-modified-since";
+const char name_36[] PROGMEM = "from";
+const char name_37[] PROGMEM = "host";
+const char name_38[] PROGMEM = "if-match";
+const char name_39[] PROGMEM = "if-modified-since";
+const char name_40[] PROGMEM = "if-none-match";
 
-const char name_41[] PROGMEM = "if-none-match";
-const char name_42[] PROGMEM = "if-range";
-const char name_43[] PROGMEM = "if-unmodified-since";
-const char name_44[] PROGMEM = "last-modified";
-const char name_45[] PROGMEM = "link";
+const char name_41[] PROGMEM = "if-range";
+const char name_42[] PROGMEM = "if-unmodified-since";
+const char name_43[] PROGMEM = "last-modified";
+const char name_44[] PROGMEM = "link";
+const char name_45[] PROGMEM = "location";
 
-const char name_46[] PROGMEM = "location";
-const char name_47[] PROGMEM = "max-forwards";
-const char name_48[] PROGMEM = "proxy-authenticate";
-const char name_49[] PROGMEM = "proxy-authorization";
-const char name_50[] PROGMEM = "range";
+const char name_46[] PROGMEM = "max-forwards";
+const char name_47[] PROGMEM = "proxy-authenticate";
+const char name_48[] PROGMEM = "proxy-authorization";
+const char name_49[] PROGMEM = "range";
+const char name_50[] PROGMEM = "referer";
 
-const char name_51[] PROGMEM = "referer";
-const char name_52[] PROGMEM = "refresh";
-const char name_53[] PROGMEM = "retry-after";
-const char name_54[] PROGMEM = "server";
-const char name_55[] PROGMEM = "set-cookie";
+const char name_51[] PROGMEM = "refresh";
+const char name_52[] PROGMEM = "retry-after";
+const char name_53[] PROGMEM = "server";
+const char name_54[] PROGMEM = "set-cookie";
+const char name_55[] PROGMEM = "strict-transport-security";
 
-const char name_56[] PROGMEM = "strict-transport-security";
-const char name_57[] PROGMEM = "transfer-encoding";
-const char name_58[] PROGMEM = "user-agent";
-const char name_59[] PROGMEM = "vary";
-const char name_60[] PROGMEM = "via";
-
-const char name_61[] PROGMEM = "www-authenticate";
-
+const char name_56[] PROGMEM = "transfer-encoding";
+const char name_57[] PROGMEM = "user-agent";
+const char name_58[] PROGMEM = "vary";
+const char name_59[] PROGMEM = "via";
+const char name_60[] PROGMEM = "www-authenticate";
+//static table header values
 
 const char value_0[] PROGMEM = "";   
 const char value_1[] PROGMEM = "GET";
@@ -324,11 +335,10 @@ const char value_58[] PROGMEM = "";
 const char value_59[] PROGMEM = "";
 const char value_60[] PROGMEM = "";
 
-const char value_61[] PROGMEM = "";
 
 // Then set up a table to refer to your strings.
 
-const char* const static_header_name_table[] PROGMEM = {name_0, name_1, name_2, name_3, name_4, name_5, name_6, name_7, name_8, name_9,name_10, name_11, name_12, name_13, name_14, name_15, name_16, name_17, name_18, name_19, name_20, name_21, name_22, name_23, name_24, name_25, name_26, name_27, name_28, name_29, name_30, name_31, name_32, name_33, name_34, name_35, name_36, name_37, name_38, name_39, name_40, name_41, name_42, name_43, name_44, name_45, name_46, name_47, name_48, name_49, name_50, name_51, name_52, name_53, name_54, name_55, name_56, name_57, name_58, name_59, name_60, name_61};
-const char* const static_header_value_table[] PROGMEM = {value_0, value_1, value_2, value_3, value_4, value_5, value_6, value_7, value_8, value_9,value_10, value_11, value_12, value_13, value_14, value_15, value_16, value_17, value_18, value_19, value_20, value_21, value_22, value_23, value_24, value_25, value_26, value_27, value_28, value_29, value_30, value_31, value_32, value_33, value_34, value_35, value_36, value_37, value_38, value_39, value_40, value_41, value_42, value_43, value_44, value_45, value_46, value_47, value_48, value_49, value_50, value_51, value_52, value_53, value_54, value_55, value_56, value_57, value_58, value_59, value_60, value_61};
+const char* const static_header_name_table[] PROGMEM = {name_0, name_1, name_2, name_3, name_4, name_5, name_6, name_7, name_8, name_9,name_10, name_11, name_12, name_13, name_14, name_15, name_16, name_17, name_18, name_19, name_20, name_21, name_22, name_23, name_24, name_25, name_26, name_27, name_28, name_29, name_30, name_31, name_32, name_33, name_34, name_35, name_36, name_37, name_38, name_39, name_40, name_41, name_42, name_43, name_44, name_45, name_46, name_47, name_48, name_49, name_50, name_51, name_52, name_53, name_54, name_55, name_56, name_57, name_58, name_59, name_60};
+const char* const static_header_value_table[] PROGMEM = {value_0, value_1, value_2, value_3, value_4, value_5, value_6, value_7, value_8, value_9,value_10, value_11, value_12, value_13, value_14, value_15, value_16, value_17, value_18, value_19, value_20, value_21, value_22, value_23, value_24, value_25, value_26, value_27, value_28, value_29, value_30, value_31, value_32, value_33, value_34, value_35, value_36, value_37, value_38, value_39, value_40, value_41, value_42, value_43, value_44, value_45, value_46, value_47, value_48, value_49, value_50, value_51, value_52, value_53, value_54, value_55, value_56, value_57, value_58, value_59, value_60};
 
 
